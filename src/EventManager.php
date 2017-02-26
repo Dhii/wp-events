83a093296623eec2d9b6203fd51e1d7a4d169f60
Anton Ukhanev <xedin.unknown@gmail.com>
2017-02-26 21:03:25 +0100

    All events invocations normalized to same signature (#5)
    All event handlers, regardless of whether they are invoked by the manager
    or by WordPress, now receive one parameter: the event instance.

--- src/EventManager.php
+++ src/EventManager.php
@@ -18,7 +18,18 @@ use Psr\EventManager\EventManagerInterface;
 class EventManager implements EventManagerInterface
 {
     /**
+     * A cache of callback wrappers, by hash.
+     *
+     * @since [*next-version*]
+     *
+     * @var callable[]
+     */
+    protected $callbackWrappers = array();
+
+    /**
      * Constructs a new instance.
+     *
+     * @since [*next-version*]
      */
     public function __construct()
     {
@@ -29,18 +40,83 @@ class EventManager implements EventManagerInterface
      *
      * @since [*next-version*]
      */
-    public function attach($event, $callback, $priority = 10)
+    public function attach($name, $callback, $priority = 10)
     {
-        $eventObject   = $this->normalizeEvent($event);
-        $numArgsToPass = $this->getCallableNumParams($callback);
-        $this->_addHook($name, $callback, $priority, 1);
+        $handler = $this->_getCallbackWrapper($name, $callback);
+        $this->_addHook($name, $handler, $priority, 1);
     }
 
+    /**
+     * Adds a hook to the environment.
+     *
+     * @since [*next-version*]
+     *
+     * @see add_filter()
+     *
+     * @param string $name
+     * @param callable $handler
+     * @param int $priority
+     * @param int $numArgs
+     * @return EventManager This instance.
+     */
     protected function _addHook($name, $handler, $priority = 10, $numArgs = 1)
     {
         \add_filter($name, $handler, $priority, $numArgs);
 
         return $this;
+    }
+
+    /**
+     * Gets a cached callback wrapper, creating it if no wrapper exists yet for given parameters.
+     *
+     * @since [*next-version*]
+     *
+     * @see _createCallbackWrapper()
+     *
+     * @param string $name
+     * @param callable $callback
+     * @return \Closure
+     */
+    protected function _getCallbackWrapper($name, $callback)
+    {
+        $cbHash = $this->_hashCallable($callback);
+        if (!isset($this->callbackWrappers[$cbHash])) {
+            $this->callbackWrappers[$cbHash] = $this->_createCallbackWrapper($name, $callback);
+        }
+
+        return $this->callbackWrappers[$cbHash];
+    }
+
+    /**
+     * Creates a wrapper for an event handler, containing and replacing it.
+     *
+     * If the wrapper receives an {@see EventInterface}, it will use that and assume it was
+     * triggered or normalized earlier by the event manager.
+     * Otherwise, it will assume that regular parameters are passed, such as
+     * what WordPress does, and will create an event object from that.
+     * Also allows events to stop propagation.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $name The name of the event, for which a wrapper is created.
+     * @param callable $callback The callback to wrap.
+     * @return \Closure The wrapper.
+     */
+    protected function _createCallbackWrapper($name, &$callback)
+    {
+        $me = $this;
+        return function() use ($name, &$callback, &$me) {
+            $args = \func_get_args();
+            $event = \count($args) && $args[0] instanceof EventInterface
+                    ? $args[0]
+                    : $me->createEvent($name, $args);
+            /* @var $event \Dhii\WpEvents\Event */
+            if (!$event->isPropagationStopped()) {
+                \call_user_func_array($callback, array($event));
+            }
+
+            return $event;
+        };
     }
 
     /**
@@ -145,6 +221,22 @@ class EventManager implements EventManagerInterface
     }
 
     /**
+     * Creates a new event instance.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $name        The event name.
+     * @param array  $params      The event parameters.
+     * @param mixed  $target      The target object. Used for context.
+     * @param bool   $propagation True to propagate the event, false to not.
+     * @return Event The new event.
+     */
+    public function createEvent($name, $params = array(), $target = null, $propagation = true)
+    {
+        return $this->_createEvent($name, $params, $target, $propagation);
+    }
+
+    /**
      * Gets the number of parameters for a callable.
      *
      * @param callable $callable The callable.
@@ -168,5 +260,86 @@ class EventManager implements EventManagerInterface
         return is_array($callable) ?
             new \ReflectionMethod($callable[0], $callable[1]) :
             new \ReflectionFunction($callable);
+    }
+
+    /**
+     * Computes a hash of a given callable.
+     *
+     * @since [*next-version*]
+     *
+     * @param callable $callable The callable to hash.
+     * @return string A hash of the callable.
+     * @throws \InvalidArgumentException If not a valid callable.
+     */
+    protected function _hashCallable($callable)
+    {
+        if (is_object($callable)) {
+            return $this->_hashObject($callable);
+        }
+
+        if (is_array($callable)) {
+            return $this->_hashArray($callable);
+        }
+
+        throw new \InvalidArgumentException('Could not hash: not a valid callback');
+    }
+
+    /**
+     * Computes a hash of the array.
+     *
+     * Accounts for nested arrays.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $array The array to hash.
+     * @return string A hash of the array.
+     */
+    protected function _hashArray(array $array)
+    {
+        $itemHashes = array();
+        foreach ($array as $_idx => $_item) {
+            if (is_array($_item)) {
+                $itemHashes[$_idx] = $this->_hashArray($_item);
+            } elseif (is_object($_item)) {
+                $itemHashes[$_idx] = $this->_hashObject($_item);
+            } elseif (is_resource($_item)) {
+                $itemHashes[$_idx] = (string) $_item;
+            } else {
+                $itemHashes[$_idx] = $_item;
+            }
+        }
+
+        $itemHashes = serialize($itemHashes);
+
+        return $this->_hashScalar($itemHashes);
+    }
+
+    /**
+     * Computes a hash of an object.
+     *
+     * The same object will always have the same hash.
+     * Different identical objects will produce different results.
+     *
+     * @since [*next-version*]
+     *
+     * @param object $object The object to hash.
+     * @return string A hash of the object.
+     */
+    protected function _hashObject($object)
+    {
+        return \spl_object_hash($object);
+    }
+
+    /**
+     * Computes a hash of a scalar value.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|int|float|bool $value The value to hash.
+     * @return string A hash of the scalar value.
+     */
+    protected function _hashScalar($value)
+    {
+        return \sha1($value);
     }
 }
